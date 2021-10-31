0658ea2b55737dc765084fbd42b9bba809f85621
BASIL SIR <92247645+Itsmebasil0@users.noreply.github.com>
2021-10-31 14:17:33 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('CONNECTION_CODE' in process.env && string === undefined) {
+        if ('CATBOT_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
