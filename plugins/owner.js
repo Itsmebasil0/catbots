0a6f86028a864e4c6b59787a300df53fc20e78b6
BASIL SIR <92247645+Itsmebasil0@users.noreply.github.com>
2021-11-04 15:41:53 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -18,7 +18,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'CatBot created by *Basil ser*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'CatBot created by *Basil sir*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '\n\n---------------------', MessageType.text);
@@ -36,7 +36,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'Pikachu Bot created by *Ameer Suhail*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'CatBot created by *Basil sir*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '*HE IS MY BOSS*', MessageType.text);
