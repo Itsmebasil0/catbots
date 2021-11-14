0219348ade79e10fd54983d94a7ade22eac30890
BASIL SIR <92247645+Itsmebasil0@users.noreply.github.com>
2021-11-14 13:30:14 +0530

    update

--- plugins/menu.js
+++ plugins/menu.js
@@ -122,7 +122,7 @@ if (config.AUTOBİO == 'true') auto_bio = 'On'
 if (config.AUTOBİO == 'false') auto_bio = 'Off'
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.jpg, contextInfo: {mentionedJid: message.mention}, caption: `╭──────────────────╮
-   ` + config.BOTPLK + `
+   ` + config.BOTSK + `
 ╭──────────────────╯
 │
 │ ʜᴇʏ ᴜsᴇʀ ` + wish + `
