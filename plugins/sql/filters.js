5f6a30913a8c45ece28435a5af1b16254ebca9ac
BASIL SIR <92247645+Itsmebasil0@users.noreply.github.com>
2021-11-19 17:49:22 +0530

    init

--- plugins/sql/filters.js
+++ plugins/sql/filters.js
@@ -1,6 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
 
-Lcensed under the  GPL-3.0 License;
+Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 WhatsAsena - Yusuf Usta
