689819866125c58ac0b00faea819c41b82539197
muhammed-usrbot <93420716+muhammed-usrbot@users.noreply.github.com>
2021-12-01 08:14:15 +0530

    Update mmpack.js

--- plugins/mmpack.js
+++ plugins/mmpack.js
@@ -3,9 +3,9 @@ const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys'
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
-const ll ="എന്തെങ്കിലും type ആക് 🙇‍♂️"
+const ll ="TYPE ANYTHING"
 const Ln = "▷🤖നിങ്ങൾ ആഗ്രഹിക്കുന്ന രീതിയിൽ മെമ്മുകൾ ഇച്ഛാനുസൃതമാക്കാൻ മെമ്മെ ലിസ്റ്റ്◁ "
-const code = "╔════◉ CAMANDS ◉════╗\n\n            ❖═List═❖\n*🔮.yasai       ❴text❵\n🔮.uddika    ❴text❵\n🔮.aah          ❴text❵\n🔮.sir            ❴text❵\n🔮.my3         ❴text❵\n🔮.fuck         ❴text❵\n🔮.hii             ❴text❵\n🔮.asai          ❴text❵\n🔮.hapoi       ❴text❵\n🔮.quby1    ❴text❵\n🔮.quby2    ❴text❵\n🔮.quby3    ❴text❵\n🔮.quby4    ❴text❵\n🔮.cat1    ❴text❵\n🔮.cat2    ❴text❵\n🔮.cat3    ❴text❵\n🔮.cat4    ❴text❵\n🔮.trollikka1    ❴text❵\n🔮.trollikka2    ❴text❵\n🔮.trollikka3    ❴text❵\n🔮.trollikka4    ❴text❵\n\n╚═══❖══▣══▣══❖═══╝\n\n▷Re Moded By: Ameer Suhail*\n*▷Specιal Thanks to ιnѕane вoy*"
+const code = "╔═════ TAURUS ═════╗\n\n            ❖═COMMANDS═❖\n\n*🍭.yasai       ❴text❵\n🍭.uddika    ❴text❵\n🍭.aah          ❴text❵\n🍭.sir            ❴text❵\n🍭.my3         ❴text❵\n🍭.fuck         ❴text❵\n🍭.hii             ❴text❵\n🍭.asai          ❴text❵\n🍭.hapoi       ❴text❵\n🍭.quby1    ❴text❵\n🍭.quby2    ❴text❵\n🍭.quby3    ❴text❵\n🍭.quby4    ❴text❵\n🍭.cat1    ❴text❵\n🍭.cat2    ❴text❵\n🍭.cat3    ❴text❵\n🍭.cat4    ❴text❵\n🍭.trollikka1    ❴text❵\n🍭.trollikka2    ❴text❵\n🍭.trollikka3    ❴text❵\n🍭.trollikka4    ❴text❵\n\n╚═══════════════╝\n\n▷TAURUS BOT*\n*▷©2021 MUHAMMED*"
 
 if (Config.WORKTYPE == 'public') {
 
