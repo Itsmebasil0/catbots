063ec7a62f4114177ab98c781397511af83f7ab6
muhammed-usrbot <93420716+muhammed-usrbot@users.noreply.github.com>
2021-12-05 20:40:32 +0530

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -1,4 +1,4 @@
-/*Copyright (C) 2021 Ameen-Rafi.
+/*Copyright (C) 2021 muhammed.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Muhammed-taurus */
