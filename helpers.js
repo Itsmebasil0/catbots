003b1491082663d41711c9c7b63b1191f30c3170
BASIL SIR <92247645+Itsmebasil0@users.noreply.github.com>
2021-12-26 12:52:27 +0530

    ᴄᴀᴛʙᴏᴛ

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *Catbot*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *Catbot*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *Catbot*:  ```" + msg + "```"
 }
 
 
